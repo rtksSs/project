c5d0fdc392742b0b9144d164a9b7369f15e0d5f2
rtksSs <123467214+rtksSs@users.noreply.github.com>
2025-06-16 13:42:34 +0900

    Update Array.h

--- project/Array.h
+++ project/Array.h
@@ -3,5 +3,5 @@
 #include <vector>
 
 std::vector<double> generateArray(int central_element_param, int random_numbers_param);
-double				      calculateSumBetweenMinMax(const std::vector<double>& array);
-void				        sortArray(std::vector<double>& array);
+double calculateSumBetweenMinMax(const std::vector<double>& array);
+void sortArray(std::vector<double>& array);
